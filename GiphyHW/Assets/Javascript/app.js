f546a154fd34859bd729135fc98f075bdde85608
Nusaxe <lesaxuns@gmail.com>
2019-10-18 16:37:52 -0500

    Add files via upload

--- GiphyHW/Assets/Javascript/app.js
+++ GiphyHW/Assets/Javascript/app.js
@@ -1,4 +1,6 @@
-$("button").on("click", function () {
+var searches = [];
+
+$(".btns").on("click", function () {
     var synth = $(this).attr("data-synth");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         synth + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=5";
@@ -26,4 +28,43 @@ $("button").on("click", function () {
                 $(".container2").prepend(gifDiv);
             }
         });
+});
+
+$("#searchBtn").on("click", function () {
+    var search = $("#newSynth").val();
+    var synth = $(this).attr("#newSynth");
+
+    console.log(search);
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        synth + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=5";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var personImage = $("<img>");
+                personImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(personImage);
+
+                $(".container2").prepend(gifDiv);
+            }
+        });
+
+    var buttonDiv = $("#btn-div");
+    var newButton = '<button data-synth="OB-8" type="button" class="btn btn-secondary btn-sm btns">' + search + '</button>';
+    console.log(newButton);
+    buttonDiv.prepend(newButton);
 });
