c7fae47f87e1249d85620b437e60a26a9fa5318c
Nusaxe <lesaxuns@gmail.com>
2019-10-18 14:46:20 -0500

    Add files via upload

--- GiphyHW/Assets/Javascript/app.js
+++ GiphyHW/Assets/Javascript/app.js
@@ -1,7 +1,29 @@
-var myRequest = new XMLHttpRequest();
-myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-myRequest.onload = function () {
-    var myData = JSON.parse(myRequest.responseText);
-    console.log(myData[0].name);
-};
-myRequest.send();
+$("button").on("click", function () {
+    var synth = $(this).attr("data-synth");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        synth + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=5";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var personImage = $("<img>");
+                personImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(personImage);
+
+                $(".container2").prepend(gifDiv);
+            }
+        });
+});
